04a90c252dc52d386a85fccdc4c42f61e2b1827a
eldarhakimov <el.hackimov@gmail.com>
2024-10-13 11:46:03 +0300

    create loacators file

---
+++ utils/locators.py
@@ -0,0 +1,59 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators:
+    """Локаторы для работы с домашней страницей"""
+
+    LINK_ACCOUNT_BUTTON = [By.XPATH, '//p[text()="Личный Кабинет"]/parent::a']  # Ссылка на личный кабинет
+
+    CHECKOUT_BUTTON= [By.XPATH, '//div/button[text()="Оформить заказ"]']  # Кнопка оформить заказ
+
+    LINK_ENTER = './/div/button[text()="Войти в аккаунт"]'  # Ссылка на форму Войти в аккаунт
+
+class AccountProfileLocators:
+
+    LINK_TO_PROFILE = [By.XPATH, '//a[text()="Профиль"]'] #Ссылка на раздел профиля в ЛК
+
+    LINK_TO_HISTORY_ORDER = [By.XPATH, '//a[text()="История заказов"]']
+
+    BUTTON_TO_EXIT = [By.XPATH, '//button[text()="Выход"]']
+
+    TEXT_IN_ACCOUNT_PROFILE = [By.XPATH, '//p[text()="В этом разделе вы можете изменить свои персональные данные"]']
+
+
+class AccountAccessLocators:
+
+    Authorization_form_title = [By.XPATH, '//h2[text()="Вход"]'] # Заголовок формы авторизации
+
+    EMAIL_INPUT_AUTHORIZATION_FORM = [By.XPATH, '//div[h2[text()="Вход"]]/descendant::label[text('
+                                                ')="Email"]/following-sibling::input']  # Поле ввода эл.почты в форме
+    # авторизации
+
+    PASSWORD_INPUT_AUTHORIZATION_FORM = [By.XPATH,'//div[h2[text()="Вход"]]/descendant::label[text('
+                                                  ')="Пароль"]/following-sibling::input']  # Поле ввода пароля в
+    # форме авторизации
+
+    AUTHORIZATION_BUTTON_AUTHORIZATION_FORM = [By.XPATH, './/form[@class="Auth_form__3qKeq mb-20"]/button[text('
+                                                         ')="Войти"]']  # Кнопка войти в форме авторизации
+
+    RESTORE_PASSWORD_BUTTON = [By.XPATH, '//p[text()="Забыли пароль?"]/a[text()="Восстановить пароль"]']
+    # Ссылка на форму восстановления пароля
+
+
+class PasswordRecoveryLocators:
+    """Локаторы для работы с функционалом по восстановлению пароля"""
+    RECOVERY_FORM_TITLE = [By.XPATH, '//h2[text()="Восстановление пароля"]'] # Заголовок формы восстановления пароля
+    EMAIL_INPUT_RECOVERY_FORM = [By.XPATH, '//div[h2[text()="Восстановление пароля"]]//descendant::label[text('
+                                           ')="Email"]/following-sibling::input'] #  Поле ввода эл.почты в форме
+    # восстановления пароля
+
+    RESTORE_PASSWORD_BUTTON = [By.XPATH, '//button[text()="Восстановить"]']
+    # Кнопка "Восстановить" в форме восстановления пароля
+
+    PASSWORD_VISIBILITY_BUTTON = [By.XPATH, '//div[contains(@class, "input__icon-action")]'] # Кнопка вкл видимости
+    # пвроля в форме восстановления пароля
+
+    SAVE_BUTTON_IN_RECOVERY_FORM_TITLE = [By.XPATH, '//button[text()="Сохранить"]'] # Кнопка "Сохраниить" в форме
+    # восстановления пароля
+
+
+
